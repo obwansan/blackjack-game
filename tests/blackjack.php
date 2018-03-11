6867013a357da2e9387c578bbed2a023f8b961f1
obwansan <kob123@hotmail.co.uk>
2018-03-11 11:06:06 +0000

    Completed dealCards() tests - passing except malformed test

--- tests/blackjack.php
+++ tests/blackjack.php
@@ -7,7 +7,7 @@ require('../blackjack.php');
 class StackTest extends TestCase
 {
 
-    /** Testing createDeck() **/
+    /**** Testing createDeck() ****/
 
     // success - check if deck is an array
     public function testcreateDeckSuccessArray()
@@ -41,7 +41,7 @@ class StackTest extends TestCase
         // Can you use in_array() to check?
     }
 
-    /** Testing dealCards() **/
+    /**** Testing dealCards() ****/
 
     // success - check if hand is an array
     public function testdealCardsSuccessArray()
@@ -104,19 +104,7 @@ class StackTest extends TestCase
         $this->assertInternalType('array', $case, $message = 'Error: Not expected data type');
     }
 
-    // success - check if number of elements in array is as expected
-    public function testdealCardsSuccessCount()
-    {
-        $deck = [
-            ['suit' => 'hearts', 'value' => '1'],
-            ['suit' => 'hearts', 'value' => '2'],
-            ['suit' => 'hearts', 'value' => '3'],
-        ];
-        $case = dealCards($deck);
-        $this->assertCount(4, $case, $message = 'Error: Less than 4 cards in hand');
-    }
-
-    // success - check that hand contains a suit and value
+    // success - check that hand contains 'suit'
     public function testdealCardsSuccessSuit()
     {
         $deck = [
@@ -175,10 +163,92 @@ class StackTest extends TestCase
         ];
         $case = dealCards($deck);
         $this->assertNotContains('suit', $case);
-        //
     }
 
+    // success - check that hand contains 'clubs'
+    public function testdealCardsSuccessSuitClubs()
+    {
+        $deck = [
+            ['suit' => 'hearts', 'value' => '1'],
+            ['suit' => 'hearts', 'value' => '2'],
+            ['suit' => 'hearts', 'value' => '3'],
+            ['suit' => 'hearts', 'value' => '4'],
+            ['suit' => 'hearts', 'value' => '5'],
+            ['suit' => 'hearts', 'value' => '6'],
+            ['suit' => 'hearts', 'value' => '7'],
+            ['suit' => 'hearts', 'value' => '8'],
+            ['suit' => 'hearts', 'value' => '9'],
+            ['suit' => 'hearts', 'value' => '10'],
+            ['suit' => 'hearts', 'value' => '10'],
+            ['suit' => 'hearts', 'value' => '10'],
+            ['suit' => 'hearts', 'value' => '10'],
+            ['suit' => 'clubs', 'value' => '1'],
+            ['suit' => 'clubs', 'value' => '2'],
+            ['suit' => 'clubs', 'value' => '3'],
+            ['suit' => 'clubs', 'value' => '4'],
+            ['suit' => 'clubs', 'value' => '5'],
+            ['suit' => 'clubs', 'value' => '6'],
+            ['suit' => 'clubs', 'value' => '7'],
+            ['suit' => 'clubs', 'value' => '8'],
+            ['suit' => 'clubs', 'value' => '9'],
+            ['suit' => 'clubs', 'value' => '10'],
+            ['suit' => 'clubs', 'value' => '10'],
+            ['suit' => 'clubs', 'value' => '10'],
+            ['suit' => 'clubs', 'value' => '10'],
+            ['suit' => 'diamonds', 'value' => '1'],
+            ['suit' => 'diamonds', 'value' => '2'],
+            ['suit' => 'diamonds', 'value' => '3'],
+            ['suit' => 'diamonds', 'value' => '4'],
+            ['suit' => 'diamonds', 'value' => '5'],
+            ['suit' => 'diamonds', 'value' => '6'],
+            ['suit' => 'diamonds', 'value' => '7'],
+            ['suit' => 'diamonds', 'value' => '8'],
+            ['suit' => 'diamonds', 'value' => '9'],
+            ['suit' => 'diamonds', 'value' => '10'],
+            ['suit' => 'diamonds', 'value' => '10'],
+            ['suit' => 'diamonds', 'value' => '10'],
+            ['suit' => 'diamonds', 'value' => '10'],
+            ['suit' => 'spades', 'value' => '1'],
+            ['suit' => 'spades', 'value' => '2'],
+            ['suit' => 'spades', 'value' => '3'],
+            ['suit' => 'spades', 'value' => '4'],
+            ['suit' => 'spades', 'value' => '5'],
+            ['suit' => 'spades', 'value' => '6'],
+            ['suit' => 'spades', 'value' => '7'],
+            ['suit' => 'spades', 'value' => '8'],
+            ['suit' => 'spades', 'value' => '9'],
+            ['suit' => 'spades', 'value' => '10'],
+            ['suit' => 'spades', 'value' => '10'],
+            ['suit' => 'spades', 'value' => '10'],
+            ['suit' => 'spades', 'value' => '10']
+        ];
+        $case = dealCards($deck);
+        $this->assertNotContains('clubs', $case);
+    }
 
+    // failure - check if number of elements in array is as expected
+    public function testdealCardsFailureCount()
+    {
+        $deck = [
+            ['suit' => 'hearts', 'value' => '1'],
+            ['suit' => 'hearts', 'value' => '2'],
+            ['suit' => 'hearts', 'value' => '3'],
+        ];
+        $case = dealCards($deck);
+        $this->assertCount(4, $case, $message = 'Error: Less than 4 cards in hand');
+    }
+
+    //malformed test
+    public function testdealCardsMalformed ()
+    {
+        $input = [];
+        $this->expectException(TypeError::class);
+        $case = dealCards($input);
+        
+        // test fails - is that supposed to happen?
+    }
+
+    /**** Testing getSCores() ****/
 
     // success
     public function testgetScoresFailureArray ()
@@ -242,13 +312,3 @@ class StackTest extends TestCase
     }
 
 }
-
-//malformed test
-//public function testmultiplyByTwoMalformed ()
-//{
-//    $input = [];
-//    $this->expectException(TypeError::class);
-//    $case = multiplyByTwo($input);
-//
-//}
-
